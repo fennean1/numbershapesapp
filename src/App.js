b0377df171b54b3113e208b75b3c5344ae0dfae9
Andrew Fenner <fennean1@gmail.com>
2022-03-26 21:29:06 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import ActivityCard from "./ActivityCard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const Home = () => (
+  <div><p>Hello!</p></div>
+)
+
 const Main = () => (
   <Routes>
     <Route path="/schematest/:item/*" element={<ActivityCard/>} />
+    <Route path="/" element={<Home/>} />
   </Routes>
 )
 
